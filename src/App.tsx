0c08ed644c5dd0384ac799803fbabb20fe527da3
Karthikeyan Sekizhar <k@kaos.agency>
2022-02-02 21:35:37 +0530

    our first component - the progress bar for the demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { ThemeProvider } from "@kaos-agency/amagi-react-design-system";
+import {
+  ProgressBar,
+  ThemeProvider,
+} from "@kaos-agency/amagi-react-design-system";
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider theme="dark" className="App">
       <h1>Hello world!</h1>
+      <div id="demo-progress"><ProgressBar progress={20} /></div>
     </ThemeProvider>
   );
 }
