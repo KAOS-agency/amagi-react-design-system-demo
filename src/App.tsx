cc186288d75c9e3bc56f5eabaeb11a74faa58655
Karthikeyan Sekizhar <k@kaos.agency>
2022-02-02 21:53:13 +0530

    added splitshell and option to collapse

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  Button,
+  Left,
   ProgressBar,
+  Right,
+  Splitshell,
   ThemeProvider,
 } from "@kaos-agency/amagi-react-design-system";
 import Header from "./header";
 import './App.css';
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(true);
   return (
     <ThemeProvider theme="dark" className="App">
       <Header />
+      <Splitshell className="splitshell">
+        <Left
+          show={showSidebar}
+          width={300}
+        >
+          <div>Hey!</div>
+        </Left>
+        <Right style={{ backgroundColor: "var(--neutral-stone-500)" }}>
+          <Button onClick={() => setShowSidebar((s) => !s)}>
+            Toggle sidebar
+          </Button>
+        </Right>
+      </Splitshell>
       <div id="demo-progress">
-        <ProgressBar progress={40} />
+        <ProgressBar progress={60} color="orange" />
       </div>
     </ThemeProvider>
   );
