4031a5008c09a36eeaa6fda74e76c16f15b13df0
Karthikeyan Sekizhar <k@kaos.agency>
2022-02-02 21:26:36 +0530

    install package and set up theme provier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { ThemeProvider } from "@kaos-agency/amagi-react-design-system";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme="dark" className="App">
       <h1>Hello world!</h1>
-    </div>
+    </ThemeProvider>
   );
 }
 
